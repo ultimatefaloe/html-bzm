6046f4757a0cfb59214f58b0d36bd2d1b20b5f54
ultimatefaloe <ultimatefaloe@outlool.com>
2026-07-02 10:15:30 +0100

    class 12 week 11

---
+++ class_11_4wk/js/main.js
@@ -0,0 +1,14 @@
+// Working with variables
+let age = 30;
+let isStudent = true;
+
+let name = "John";
+const email = "john@example.com";
+name = "Jane"; // Reassigning the value of name variable
+// email = "jane@example.com"; // This would cause an error since email is a const
+
+
+console.log(name); // Output: Jane
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
